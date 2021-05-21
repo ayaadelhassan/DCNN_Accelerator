e629508c04f450642450e9ac780f475620b60f92
aashrafh <aashrafh31@gmail.com>
2021-05-22 00:15:13 +0200

    Write tests for pooling image

--- CNN/pool_image.sv
+++ CNN/pool_image.sv
@@ -10,14 +10,14 @@ module pool_image(image, pooled_image);
 	genvar i,j;
 	generate 
 
-	for (i = 0; i < n-2; i += 2)
+	for (i = 0; i < n ; i += 2)
 	begin
-		for(j = 0; j < n-2; j += 2)
+		for(j = 0; j < n; j += 2)
 		begin
-			assign window[j+(n-2)*i][0:1] = image[j+(n*i):j+(n*i)+1];
-			assign window[j+(n-2)*i][2:3] = image[j+(n*(i+1)):j+(n*(i+1))+1];
+			assign window[j/2+n*i/4][0:1] = image[j+(n*i):j+(n*i)+1];
+			assign window[j/2+n*i/4][2:3] = image[j+(n*(i+1)):j+(n*(i+1))+1];
 
-          		pool_window pw(window[j+(n-2)*i], pooled_image[j+(n-2)*i]);
+          		pool_window pw(window[j/2+n*i/4], pooled_image[j/2+n*i/4]);
 		end
 	end
 	
