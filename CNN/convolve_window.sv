5ad93562833f5a07c48cd090d3e619fd1bab64ea
IslamAhmed1092 <eslam1092@hotmail.com>
2021-05-21 19:40:41 +0200

    change convolve window to handle 1d array

--- CNN/convolve_window.sv
+++ CNN/convolve_window.sv
@@ -1,6 +1,6 @@
 module convolve_window(window, filter, value);
-	input signed [15:0] window [0:4][0:4];
-	input signed [15:0] filter [0:4][0:4]; 
+	input signed [15:0] window [0:24];
+	input signed [15:0] filter [0:24]; 
 	output signed [15:0] value;
 	
 	wire signed [15:0] temp [0:24];
@@ -8,12 +8,9 @@ module convolve_window(window, filter, value);
 	genvar i,j;
 	generate 
 
-	for (i=0; i<5; i++)
+	for (i=0; i<25; i++)
 	begin
-      		for (j=0; j<5; j++)
-		begin
-          		multiplication mp(window[i][j], filter[i][j], temp[i * 5 + j]);
-		end
+          	multiplication mp(window[i], filter[i], temp[i]);
 	end
 	
 	endgenerate
