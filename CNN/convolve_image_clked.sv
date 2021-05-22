09b94af1eb98aeb94d2eb327f714cd9988a2e500
IslamAhmed1092 <eslam1092@hotmail.com>
2021-05-22 05:41:27 +0200

    finish convolve_image_clked and testing it

--- CNN/convolve_image_clked.sv
+++ CNN/convolve_image_clked.sv
@@ -1,37 +1,49 @@
-module convolve_image_clked(image, filter, convolved_image, clk, reset ,enable, done);
+module convolve_image_clked(image, filter, convolved, clk, reset ,enable, done);
 	
-	parameter n = 32;
+	parameter n = 10;
 
 	input signed [15:0] image [0:n*n-1];
 	input signed [15:0] filter [0:24];
-    input clk , reset, enable; 
-	output signed [15:0] convolved_image;
-    output done; 
-    reg done; 
+    	input clk , reset, enable; 
+	output signed [15:0] convolved;
+   	output done; 
+    	reg done; 
 	reg signed [15:0] window [0:24];
-
-    convolve_window cw(window, filter, convolved_image);
+    	
+	convolve_window cw(window, filter, convolved);
     
-	integer i = 0,j = 0;
-    always @(posedge clk, reset) begin
-        if (reset == 1)begin
-            i  = 0; 
-            j = 0 ; 
-            window = {25{16'b0}}; 
-        end
-        if(enable == 1)
-        begin
-            if (i == and j == )begin
-                done = 1; 
-            end
-            window[0:4] = image[j+(n*i):j+(n*i)+4];
-            window[5:9] = image[j+(n*(i+1)):j+(n*(i+1))+4];
-            window[10:14] = image[j+(n*(i+2)):j+(n*(i+2))+4];
-            window[15:19] = image[j+(n*(i+3)):j+(n*(i+3))+4];
-            window[20:24] = image[j+(n*(i+4)):j+(n*(i+4))+4];
-            j = j + 1; 
-            i = i + 1; 
-    end
+	integer i,j,k,f;
+    	always @(posedge clk, reset) 
+	begin
+        	if (reset) begin
+        	    	i  <= 0; 
+        	    	j <= 0 ;
+			done <= 0;
+			for(k=0;k<25;k=k+1) begin
+        	    		window[k] <= 0;
+			end
+        	end if(enable) begin
+			f = 0;
+			
+			for(integer x=i;x<i+5;x++) begin
+				for(integer y=0;y<5;y++) begin
+					window[f] = image[j+(n*x)+y];
+					f = f + 1;
+				end
+			end
+        		            
+            		j = j + 1; 
+			
+			if(j >= n - 4) begin
+				i = i + 1;
+				j = 0;
+			end
+
+			if(i >= n - 4) begin
+				done = 1;	
+			end
+		end 
+    	end
 	
 
 endmodule
